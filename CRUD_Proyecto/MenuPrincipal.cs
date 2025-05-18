f6498f22c1338e75f5892fd691d2fe17cd96c415
xJoseph777x <126911724+xJoseph777x@users.noreply.github.com>
2025-05-18 16:32:48 -0600

    Agregando el login fase 1

---
+++ CRUD_Proyecto/MenuPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Proyecto
+{
+    public partial class MenuPrincipal : Form
+    {
+        private int tipoUsuario;
+
+        public MenuPrincipal(int tipo)
+        {
+            InitializeComponent();
+            tipoUsuario = tipo;
+        }
+
+        private void MenuPrincipal_Load(object sender, EventArgs e)
+        {
+            if (tipoUsuario == 1)
+                lb_Tipo.Text = "Administrador";
+            else if (tipoUsuario == 2)
+                lb_Tipo.Text = "Médico";
+            else if (tipoUsuario == 3)
+                lb_Tipo.Text = "Recepcionista";
+        }
+    }
+}
