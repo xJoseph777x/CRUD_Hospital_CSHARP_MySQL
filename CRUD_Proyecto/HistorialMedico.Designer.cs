7d32685634b39bb306efd87ffb2b32fc37a3179f
RIVERMOLD <uhector50@gmail.com>
2025-05-23 23:55:26 -0600

    Ajustes

--- CRUD_Proyecto/HistorialMedico.Designer.cs
+++ CRUD_Proyecto/HistorialMedico.Designer.cs
@@ -35,15 +35,17 @@
             this.label2 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.labelFecha = new System.Windows.Forms.Label();
+            this.btnRegresar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_pacientes)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(225, 38);
+            this.label1.Location = new System.Drawing.Point(300, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.Size = new System.Drawing.Size(142, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Escoga un tipo de filtro";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -52,9 +54,10 @@
             // PacienteEntry
             // 
             this.PacienteEntry.Enabled = false;
-            this.PacienteEntry.Location = new System.Drawing.Point(339, 35);
+            this.PacienteEntry.Location = new System.Drawing.Point(452, 43);
+            this.PacienteEntry.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PacienteEntry.Name = "PacienteEntry";
-            this.PacienteEntry.Size = new System.Drawing.Size(175, 20);
+            this.PacienteEntry.Size = new System.Drawing.Size(232, 22);
             this.PacienteEntry.TabIndex = 1;
             this.PacienteEntry.TextChanged += new System.EventHandler(this.PacienteEntry_TextChanged);
             // 
@@ -62,9 +65,11 @@
             // 
             this.dgv_pacientes.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dgv_pacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_pacientes.Location = new System.Drawing.Point(12, 135);
+            this.dgv_pacientes.Location = new System.Drawing.Point(16, 166);
+            this.dgv_pacientes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgv_pacientes.Name = "dgv_pacientes";
-            this.dgv_pacientes.Size = new System.Drawing.Size(776, 303);
+            this.dgv_pacientes.RowHeadersWidth = 51;
+            this.dgv_pacientes.Size = new System.Drawing.Size(1035, 373);
             this.dgv_pacientes.TabIndex = 2;
             this.dgv_pacientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_pacientes_CellContentClick);
             // 
@@ -76,27 +81,30 @@
             "Paciente",
             "Fecha",
             "Medico"});
-            this.comboBox1.Location = new System.Drawing.Point(339, 61);
+            this.comboBox1.Location = new System.Drawing.Point(452, 75);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(282, 65);
+            this.label2.Location = new System.Drawing.Point(376, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(66, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Filtrar por:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(339, 88);
+            this.dateTimePicker1.Location = new System.Drawing.Point(452, 108);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker1.TabIndex = 5;
             this.dateTimePicker1.Value = new System.DateTime(2025, 5, 22, 1, 47, 35, 0);
             this.dateTimePicker1.Visible = false;
@@ -105,18 +113,31 @@
             // labelFecha
             // 
             this.labelFecha.AutoSize = true;
-            this.labelFecha.Location = new System.Drawing.Point(241, 91);
+            this.labelFecha.Location = new System.Drawing.Point(321, 112);
+            this.labelFecha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelFecha.Name = "labelFecha";
-            this.labelFecha.Size = new System.Drawing.Size(97, 13);
+            this.labelFecha.Size = new System.Drawing.Size(118, 16);
             this.labelFecha.TabIndex = 6;
             this.labelFecha.Text = "Escoga una fecha:";
             this.labelFecha.Visible = false;
             // 
+            // btnRegresar
+            // 
+            this.btnRegresar.AutoSize = true;
+            this.btnRegresar.Location = new System.Drawing.Point(30, 37);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 26);
+            this.btnRegresar.TabIndex = 7;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
             // HistorialMedico
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.labelFecha);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label2);
@@ -124,6 +145,7 @@
             this.Controls.Add(this.dgv_pacientes);
             this.Controls.Add(this.PacienteEntry);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "HistorialMedico";
             this.Text = "HistorialMedico";
             ((System.ComponentModel.ISupportInitialize)(this.dgv_pacientes)).EndInit();
@@ -141,5 +163,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label labelFecha;
+        private System.Windows.Forms.Button btnRegresar;
     }
 }
